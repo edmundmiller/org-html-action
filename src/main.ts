4c02d17835c8c7e1c667405c373801ddf10247dd
Edmund Miller <edmund.a.miller@protonmail.com>
2019-11-20 20:08:13 -0600

    feat: add initial using GitLab pages example

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,42 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from "@actions/core";
 import * as exec from "@actions/exec";
+import * as io from "@actions/io";
+import { wait } from "./wait";
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const emacsPath: string = await io.which("emacs", false);
+    if (emacsPath === "") {
+      core.startGroup("Test Emacs version");
+      core.info("Running 'emacs -version'");
+      await exec.exec("emacs", ["-version"]);
+      core.endGroup();
+    }
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    core.startGroup("Check org version");
+    core.info("Running 'emacs -version'");
+    await exec.exec("emacs", [
+      "--batch",
+      "--no-init-file",
+      "--funcall",
+      "org-version"
+    ]);
+    // TODO check that org is >= 8
+    core.endGroup();
 
-    core.setOutput('time', new Date().toTimeString());
+    core.startGroup("Build docs");
+    core.info(
+      "Running 'emacs --batch --no-init-file --load publish.el --funcall org-publish-all'"
+    );
+    await exec.exec("emacs", [
+      "--batch",
+      "--no-init-file",
+      "--load",
+      "publish.el",
+      "--funcall",
+      "org-publish-all"
+    ]);
+    core.endGroup();
   } catch (error) {
     core.setFailed(error.message);
   }
---
+++ src/publish.el
@@ -0,0 +1,68 @@
+;; publish.el --- Publish org-mode project on Gitlab Pages
+;; Author: Rasmus
+
+;;; Commentary:
+;; This script will convert the org-mode files in this directory into
+;; html.
+
+;;; Code:
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-refresh-contents)
+(package-install 'org-plus-contrib)
+(package-install 'htmlize)
+
+(require 'org)
+(require 'ox-publish)
+
+;; setting to nil, avoids "Author: x" at the bottom
+(setq user-full-name nil)
+
+(setq org-export-with-section-numbers nil
+      org-export-with-smart-quotes t
+      org-export-with-toc nil)
+
+(setq org-html-divs '((preamble "header" "top")
+                      (content "main" "content")
+                      (postamble "footer" "postamble"))
+      org-html-container-element "section"
+      org-html-metadata-timestamp-format "%Y-%m-%d"
+      org-html-checkbox-type 'html
+      org-html-html5-fancy t
+      org-html-validation-link nil
+      org-html-doctype "html5")
+
+(defvar site-attachments
+  (regexp-opt '("jpg" "jpeg" "gif" "png" "svg"
+                "ico" "cur" "css" "js" "woff" "html" "pdf"))
+  "File types that are published as static files.")
+
+(setq org-publish-project-alist
+      (list
+       (list "site-org"
+             :base-directory "docs"
+             :base-extension "org"
+             :recursive t
+             :publishing-function '(org-html-publish-to-html)
+             :publishing-directory "./public"
+             :exclude (regexp-opt '("README" "draft"))
+             :auto-sitemap t
+             :sitemap-filename "index.org"
+             :sitemap-file-entry-format "%d *%t*"
+             :html-head-extra "<link rel=\"icon\" type=\"image/x-icon\" href=\"/favicon.ico\"/>"
+             :sitemap-style 'list
+             :sitemap-sort-files 'anti-chronologically)
+       (list "site-static"
+             :base-directory "."
+             :exclude "public/"
+             :base-extension site-attachments
+             :publishing-directory "./public"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+       (list "site" :components '("site-org"))))
+
+(provide 'publish)
+;;; publish.el ends here
